0b01ed58b8c6174680427feec25ec12d8993ac22
MATI UL REHMAN <iammatiz00@gmail.com>
2023-03-11 19:19:07 +0500

    text widget added;

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+//text widget
+      const Text(
+    "Text Widget",
+    textDirection: TextDirection.ltr,
+    textAlign: TextAlign.center,
+  ));
+}
